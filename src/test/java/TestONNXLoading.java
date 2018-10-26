1ad93cf5776fe46c87e1bac6ac5b62e7c290334b
isaacmg <igodfried@isaac26.com>
2018-10-26 12:41:30 -0400

    added changes to ut
    Added unit test setup and junit to file.

--- src/test/java/TestONNXLoading.java
+++ src/test/java/TestONNXLoading.java
@@ -1,8 +1,20 @@
 import org.bytedeco.javacpp.*;
+import org.junit.Test;
+
 import static org.bytedeco.javacpp.onnx.*;
+import static org.junit.Assert.assertEquals;
+
 import java.nio.file.*;
+
+
 public class TestONNXLoading{
    LoadModelONNX chest_model =  new LoadModelONNX("chexnet-py.onnx");
+
+   @Test
+   public void testModelLoaded(){
+      assertEquals(1, 1);
+
+   }
 
 }
 
