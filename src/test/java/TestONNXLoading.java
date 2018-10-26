98af87bacb05a7288cdb8341c18f4e5a094b7ccb
isaacmg <igodfried@isaac26.com>
2018-10-26 12:45:32 -0400

    print ln

--- src/test/java/TestONNXLoading.java
+++ src/test/java/TestONNXLoading.java
@@ -9,6 +9,7 @@ import java.nio.file.*;
 
 public class TestONNXLoading{
    LoadModelONNX chest_model =  new LoadModelONNX("chexnet-py.onnx");
+   System.out.println("Model loaded")
 
    @Test
    public void testModelLoaded(){
