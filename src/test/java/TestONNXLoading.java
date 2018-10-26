b52d24f431a7583698655e03dfd8d150ba6a84d5
isaacmg <igodfried@isaac26.com>
2018-10-26 12:25:30 -0400

    Test loading class

---
+++ src/test/java/TestONNXLoading.java
@@ -0,0 +1,9 @@
+import org.bytedeco.javacpp.*;
+import static org.bytedeco.javacpp.onnx.*;
+import java.nio.file.*;
+public class TestONNXLoading{
+   LoadModelONNX chest_model =  new LoadModelONNX("chexnet-py.onnx");
+
+}
+
+
