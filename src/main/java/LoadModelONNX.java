1a729057553a88be376ab5d76cb8aede528e4e02
isaacmg <igodfried@isaac26.com>
2018-10-26 11:47:09 -0400

    Initial commit

---
+++ src/main/java/LoadModelONNX.java
@@ -0,0 +1,27 @@
+import org.bytedeco.javacpp.*;
+import static org.bytedeco.javacpp.onnx.*;
+import java.nio.file.*;
+public class LoadModelONNX {
+    LoadModelONNX(String weight_path){
+        OpSchemaVector allSchemas = OpSchemaRegistry.get_all_schemas();
+        System.out.println(allSchemas.size());
+        try{
+            byte[] bytes = Files.readAllBytes(Paths.get("examples/resources/single_relu.onnx"));
+            ModelProto model = new ModelProto();
+
+            ParseProtoFromBytes(model, new BytePointer(bytes), bytes.length);
+
+            System.out.println(model.graph().input_size());
+        }
+        catch (java.io.IOException e){
+            System.out.println(e);
+
+        }
+
+
+
+
+    }
+
+}
+
