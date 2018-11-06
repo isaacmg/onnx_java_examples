973ac722ca22577d3bb892587a022f5036f451ae
isaacmg <igodfried@isaac26.com>
2018-11-05 23:04:22 -0500

    travis ci basic test

--- src/main/java/ONNXJep.java
+++ src/main/java/ONNXJep.java
@@ -4,10 +4,14 @@ import jep.Jep;
 public class ONNXJep {
     ONNXJep(String weightPath, String modelName){
         try{
+            // Load Jep
             Jep jep = new Jep();
-            jep.eval("from test.py import TestClass");
-            jep.eval("t = TestClass()");
-            jep.eval("t.result()");
+            jep.eval("from test.py import SemanticModel");
+            jep.eval("t = SemanticModel()");
+            jep.set("arg", "his name is jeff");
+            jep.eval("t.predict(arg)");
+            Object result1 = jep.getValue("x");
+            System.out.println(result1);
 
         }
         catch(Exception e){
--- src/main/java/test.py
+++ src/main/java/test.py
@@ -1,3 +1,9 @@
-class TestClass():
-    def result(self):
-        print("Here is the result")
+from allennlp.predictors.predictor import Predictor
+class SemanticModel():
+    def __init__():
+        self.predictor = Predictor.from_path("https://s3-us-west-2.amazonaws.com/allennlp/models/srl-model-2018.05.25.tar.gz")
+
+    def predict(self, text):
+        result = self.predictor.predict(sentence=text)
+        return result
+
